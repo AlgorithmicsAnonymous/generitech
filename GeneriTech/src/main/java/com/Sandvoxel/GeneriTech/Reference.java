adc97e9168635366409c012c97162335cb2bbebb
mailboxkiller <kovalchickzz@gmail.com>
2016-05-02 20:21:22 -0500

    added so main files

---
+++ GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/Reference.java
@@ -0,0 +1,16 @@
+package com.Sandvoxel.GeneriTech;
+
+/**
+ * Created by koval on 5/2/2016.
+ */
+public class Reference {
+
+    public static final String MODID = "GeneriTech";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "GeneriTech";
+
+
+
+
+
+}
