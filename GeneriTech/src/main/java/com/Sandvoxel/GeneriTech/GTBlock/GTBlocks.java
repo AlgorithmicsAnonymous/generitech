12751c248739d148d98ab8bfb4b2244ec56fd513
CrazyGrape <crazygrape@protonmail.com>
2016-05-07 18:50:42 -0500

    Changed some things

--- GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/GTBlock/GTBlocks.java
+++ GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/GTBlock/GTBlocks.java
@@ -2,10 +2,6 @@ package com.Sandvoxel.GeneriTech.GTBlock;
 
 import com.Sandvoxel.GeneriTech.Reference;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
