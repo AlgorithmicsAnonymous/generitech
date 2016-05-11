9db207eea96f2822c7ba369e85b303ef54649196
mailboxkiller <kovalchickzz@gmail.com>
2016-05-10 20:14:18 -0500

    fixed contaner needs Gui

--- GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/TileEntitys/TileEntityPulverizer.java
+++ GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/TileEntitys/TileEntityPulverizer.java
@@ -2,6 +2,7 @@ package com.Sandvoxel.GeneriTech.TileEntitys;
 
 import com.Sandvoxel.GeneriTech.Contaners.PulverizerContaner;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockFurnace;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,6 +15,7 @@ import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntityLockable;
+import net.minecraft.tileentity.TileEntityLockableLoot;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.MathHelper;
@@ -23,39 +25,112 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 /**
  * Created by koval on 5/9/2016.
  */
-public class TileEntityPulverizer extends TileEntityLockable {
+public class TileEntityPulverizer extends TileEntityLockableLoot {
 
-
-    private static final int[] slotsTop = new int[] {0};
-    private static final int[] slotsBottom = new int[] {2, 1};
-    private static final int[] slotsSides = new int[] {1};
     private ItemStack[] PulverizerItemStacks = new ItemStack[3];
     private String PulverizerCustomName;
 
 
+
+
+
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        this.PulverizerItemStacks = new ItemStack[this.getSizeInventory()];
+
+        if (compound.hasKey("PulverizerCustomName", 8))
+        {
+            this.PulverizerCustomName = compound.getString("PulverizerCustomName");
+        }
+
+        if (!this.checkLootAndRead(compound))
+        {
+            NBTTagList nbttaglist = compound.getTagList("PulverizerItems", 10);
+
+            for (int i = 0; i < nbttaglist.tagCount(); ++i)
+            {
+                NBTTagCompound nbttagcompound = nbttaglist.getCompoundTagAt(i);
+                int j = nbttagcompound.getByte("PulverizerSlot") & 255;
+
+                if (j >= 0 && j < this.PulverizerItemStacks.length)
+                {
+                    this.PulverizerItemStacks[j] = ItemStack.loadItemStackFromNBT(nbttagcompound);
+                }
+            }
+        }
+    }
+
+    public void writeToNBT(NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+
+        if (!this.checkLootAndWrite(compound))
+        {
+            NBTTagList nbttaglist = new NBTTagList();
+
+            for (int i = 0; i < this.PulverizerItemStacks.length; ++i)
+            {
+                if (this.PulverizerItemStacks[i] != null)
+                {
+                    NBTTagCompound nbttagcompound = new NBTTagCompound();
+                    nbttagcompound.setByte("PulverizerSlot", (byte)i);
+                    this.PulverizerItemStacks[i].writeToNBT(nbttagcompound);
+                    nbttaglist.appendTag(nbttagcompound);
+                }
+            }
+
+            compound.setTag("PulverizerItems", nbttaglist);
+        }
+
+        if (this.hasCustomName())
+        {
+            compound.setString("PulverizerCustomName", this.PulverizerCustomName);
+        }
+    }
+
+
     @Override
     public int getSizeInventory() {
-        return this.PulverizerItemStacks.length;
+        return 3;
     }
 
     @Override
     public ItemStack getStackInSlot(int index) {
+        this.fillWithLoot((EntityPlayer)null);
         return this.PulverizerItemStacks[index];
     }
 
     @Override
     public ItemStack decrStackSize(int index, int count) {
-        return ItemStackHelper.func_188382_a(this.PulverizerItemStacks, index, count);
+        this.fillWithLoot((EntityPlayer)null);
+        ItemStack itemstack = ItemStackHelper.func_188382_a(this.PulverizerItemStacks, index, count);
+
+        if (itemstack != null)
+        {
+            this.markDirty();
+        }
+
+        return itemstack;
     }
 
     @Override
     public ItemStack removeStackFromSlot(int index) {
+        this.fillWithLoot((EntityPlayer)null);
         return ItemStackHelper.func_188383_a(this.PulverizerItemStacks, index);
     }
 
     @Override
     public void setInventorySlotContents(int index, ItemStack stack) {
-        stack.stackSize = this.getInventoryStackLimit();
+        this.fillWithLoot((EntityPlayer)null);
+        this.PulverizerItemStacks[index] = stack;
+
+        if (stack != null && stack.stackSize > this.getInventoryStackLimit())
+        {
+            stack.stackSize = this.getInventoryStackLimit();
+        }
+
+        this.markDirty();
     }
 
     @Override
@@ -95,27 +170,27 @@ public class TileEntityPulverizer extends TileEntityLockable {
 
     @Override
     public int getFieldCount() {
-        return 4;
+        return 0;
     }
 
     @Override
     public void clear() {
+        this.fillWithLoot((EntityPlayer)null);
+
+        for (int i = 0; i < this.PulverizerItemStacks.length; ++i)
         {
-            for (int i = 0; i < this.PulverizerItemStacks.length; ++i)
-            {
-                this.PulverizerItemStacks[i] = null;
-            }
+            this.PulverizerItemStacks[i] = null;
         }
     }
 
     @Override
     public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
-        return new PulverizerContaner(playerInventory, this);
+        return null;
     }
 
     @Override
     public String getGuiID() {
-        return "generitech:gui/furnace";
+        return null;
     }
 
     @Override
@@ -127,76 +202,5 @@ public class TileEntityPulverizer extends TileEntityLockable {
     public boolean hasCustomName() {
         return this.PulverizerCustomName != null && !this.PulverizerCustomName.isEmpty();
     }
-
-
-    public void readFromNBT(NBTTagCompound compound)
-    {
-        super.readFromNBT(compound);
-        NBTTagList nbttaglist = compound.getTagList("Items", 10);
-        this.PulverizerItemStacks = new ItemStack[this.getSizeInventory()];
-
-        for (int i = 0; i < nbttaglist.tagCount(); ++i)
-        {
-            NBTTagCompound nbttagcompound = nbttaglist.getCompoundTagAt(i);
-            int j = nbttagcompound.getByte("Slot");
-
-            if (j >= 0 && j < this.PulverizerItemStacks.length)
-            {
-                this.PulverizerItemStacks[j] = ItemStack.loadItemStackFromNBT(nbttagcompound);
-            }
-        }
-
-        if (compound.hasKey("CustomName", 8))
-        {
-            this.PulverizerCustomName = compound.getString("CustomName");
-        }
-    }
-
-    public void writeToNBT(NBTTagCompound compound)
-    {
-        super.writeToNBT(compound);
-        NBTTagList nbttaglist = new NBTTagList();
-
-        for (int i = 0; i < this.PulverizerItemStacks.length; ++i)
-        {
-            if (this.PulverizerItemStacks[i] != null)
-            {
-                NBTTagCompound nbttagcompound = new NBTTagCompound();
-                nbttagcompound.setByte("Slot", (byte)i);
-                this.PulverizerItemStacks[i].writeToNBT(nbttagcompound);
-                nbttaglist.appendTag(nbttagcompound);
-            }
-        }
-
-        compound.setTag("Items", nbttaglist);
-
-        if (this.hasCustomName())
-        {
-            compound.setString("CustomName", this.PulverizerCustomName);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
