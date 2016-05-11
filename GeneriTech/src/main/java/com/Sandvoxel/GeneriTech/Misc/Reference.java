4f97623bcf33a0c1be36dfd35283ddd260f8e3dc
Sean Latimer <seanlatimer@outlook.com>
2016-05-11 19:32:49 +0100

    Refactored Mod Name

--- GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/Misc/Reference.java
+++ GeneriTech/src/main/java/com/Sandvoxel/GeneriTech/Misc/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 
     public static final String MOD_ID = "generitech";
     public static final String VERSION = "1.0";
-    public static final String NAME = "GeneriTech";
+    public static final String MOD_NAME = "GeneriTech";
     public static final String CLIENT_PROXY_CLASS = "com.Sandvoxel.GeneriTech.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.Sandvoxel.GeneriTech.proxy.CommonProxy";
 }
