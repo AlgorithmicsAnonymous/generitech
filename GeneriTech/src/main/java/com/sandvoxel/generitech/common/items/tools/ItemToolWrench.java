77e3e54c5fafe6e1a700ee073bc225db848effb9
Sean Latimer <seanlatimer@outlook.com>
2016-05-14 23:09:03 +0100

    Wrench Rotates Blocks

--- GeneriTech/src/main/java/com/sandvoxel/generitech/common/items/tools/ItemToolWrench.java
+++ GeneriTech/src/main/java/com/sandvoxel/generitech/common/items/tools/ItemToolWrench.java
@@ -4,20 +4,26 @@ import com.google.common.collect.Sets;
 import com.sandvoxel.generitech.GeneriTechTabs;
 import com.sandvoxel.generitech.common.items.ItemBaseTool;
 import com.sandvoxel.generitech.common.items.Items;
+import com.sandvoxel.generitech.common.util.IProvideEvent;
 import com.sandvoxel.generitech.common.util.IProvideRecipe;
+import com.sandvoxel.generitech.common.util.Platform;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import java.util.Set;
 
-public class ItemToolWrench extends ItemBaseTool implements IProvideRecipe {
+public class ItemToolWrench extends ItemBaseTool implements IProvideRecipe, IProvideEvent {
 
     public static Set blocksEffectiveAgainst = Sets.newHashSet(new Block[]{});
     public static boolean canHarvest = false;
@@ -34,6 +40,23 @@ public class ItemToolWrench extends ItemBaseTool implements IProvideRecipe {
     @Override
     public boolean doesSneakBypassUse(ItemStack stack, IBlockAccess world, BlockPos pos, EntityPlayer player) {
         return true;
+    }
+
+    @Override
+    public EnumActionResult onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
+        Block block = world.getBlockState(pos).getBlock();
+
+        if (block != null && !player.isSneaking()) {
+            if (Platform.isClient())
+                return !world.isRemote ? EnumActionResult.FAIL : EnumActionResult.PASS;
+
+            if (block.rotateBlock(world, pos, side)) {
+                player.swingArm(hand);
+                return !world.isRemote ? EnumActionResult.FAIL : EnumActionResult.PASS;
+            }
+        }
+
+        return EnumActionResult.FAIL;
     }
 
     @Override
